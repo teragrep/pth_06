1e520f6d0734176f90de93385ba932daefb2f2c2
Motoko Kusanagi <kordex@gmail.com>
2024-09-05 16:06:16 +0300

    change license check condition for add_headers_to_generated.sh

--- add_headers_to_generated.sh
+++ add_headers_to_generated.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 find src/main/java/com/teragrep/pth_06/jooq/generated -type f -name "*.java" -print0 | while read -r -d $'\0' file
 do
-    if ! grep -q "https://github.com/teragrep/teragrep/blob/main/LICENSE" "${file}"; then
+    if ! grep -q "Copyright (C) .* Suomen Kanuuna Oy" "${file}"; then
         cat license-header > "${file}.tmp";
         cat "${file}" >> "${file}.tmp";
         mv -f "${file}.tmp" "${file}";
