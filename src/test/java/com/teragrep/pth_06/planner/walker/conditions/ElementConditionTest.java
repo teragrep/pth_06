07acaa6b3c9a5afd72d3e726fc43b962b5d3db6b
elliVM <126466762+elliVM@users.noreply.github.com>
2024-09-05 15:47:59 +0300

    fix file licenses and apply spotless  (#79)
    * add license to missing files, remove double licenses after package import
    
    * apply spotless

--- src/test/java/com/teragrep/pth_06/planner/walker/conditions/ElementConditionTest.java
+++ src/test/java/com/teragrep/pth_06/planner/walker/conditions/ElementConditionTest.java
@@ -1,3 +1,48 @@
+/*
+ * Teragrep Archive Datasource (pth_06)
+ * Copyright (C) 2021-2024 Suomen Kanuuna Oy
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
+ *
+ *
+ * Additional permission under GNU Affero General Public License version 3
+ * section 7
+ *
+ * If you modify this Program, or any covered work, by linking or combining it
+ * with other code, such other code is not for that reason alone subject to any
+ * of the requirements of the GNU Affero GPL version 3 as long as this Program
+ * is the same Program as licensed from Suomen Kanuuna Oy without any additional
+ * modifications.
+ *
+ * Supplemented terms under GNU Affero General Public License version 3
+ * section 7
+ *
+ * Origin of the software must be attributed to Suomen Kanuuna Oy. Any modified
+ * versions must be marked as "Modified version of" The Program.
+ *
+ * Names of the licensors and authors may not be used for publicity purposes.
+ *
+ * No rights are granted for use of trade names, trademarks, or service marks
+ * which are in The Program if any.
+ *
+ * Licensee must indemnify licensors and authors for any liability that these
+ * contractual assumptions impose on licensors and authors.
+ *
+ * To the extent this program is licensed as part of the Commercial versions of
+ * Teragrep, the applicable Commercial License may apply to this file if you as
+ * a licensee so wish it.
+ */
 package com.teragrep.pth_06.planner.walker.conditions;
 
 import com.teragrep.pth_06.config.ConditionConfig;
@@ -15,6 +60,7 @@ import org.w3c.dom.Element;
 import javax.xml.parsers.DocumentBuilderFactory;
 
 class ElementConditionTest {
+
     final DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
     final Document document = Assertions.assertDoesNotThrow(() -> factory.newDocumentBuilder().newDocument());
     final DSLContext mockCtx = DSL.using(new MockConnection(ctx -> new MockResult[0]));
@@ -23,7 +69,9 @@ class ElementConditionTest {
 
     @Test
     void testStreamTags() {
-        String[] streamTags = {"index", "host", "sourcetype"};
+        String[] streamTags = {
+                "index", "host", "sourcetype"
+        };
         int loops = 0;
         for (String tag : streamTags) {
             Element element = document.createElement(tag);
@@ -44,7 +92,8 @@ class ElementConditionTest {
         Element element2 = document.createElement("indexstatement");
         element2.setAttribute("value", "searchTerm");
         element2.setAttribute("operation", "NOT_EQUALS");
-        Assertions.assertThrows(SQLDialectNotSupportedException.class, new ElementCondition(element, config)::condition);
+        Assertions
+                .assertThrows(SQLDialectNotSupportedException.class, new ElementCondition(element, config)::condition);
         Assertions.assertThrows(IllegalStateException.class, new ElementCondition(element, streamConfig)::condition);
         Assertions.assertThrows(IllegalStateException.class, new ElementCondition(element2, config)::condition);
     }
@@ -71,7 +120,9 @@ class ElementConditionTest {
 
     @Test
     void testTimeQualifiers() {
-        String[] tags = {"earliest", "latest", "index_earliest", "index_latest"};
+        String[] tags = {
+                "earliest", "latest", "index_earliest", "index_latest"
+        };
         int loops = 0;
         for (String tag : tags) {
             Element element = document.createElement(tag);
@@ -86,15 +137,16 @@ class ElementConditionTest {
 
     @Test
     void testInvalidStreamTags() {
-        String[] tags = {"earliest", "latest", "index_earliest", "index_latest", "indexstatement"};
+        String[] tags = {
+                "earliest", "latest", "index_earliest", "index_latest", "indexstatement"
+        };
         int loops = 0;
         for (String tag : tags) {
             Element element = document.createElement(tag);
             element.setAttribute("value", "1000");
             element.setAttribute("operation", "EQUALS");
-            Assertions.assertThrows(IllegalStateException.class,
-                    () -> new ElementCondition(element, streamConfig).condition()
-            );
+            Assertions
+                    .assertThrows(IllegalStateException.class, () -> new ElementCondition(element, streamConfig).condition());
             loops++;
         }
         Assertions.assertEquals(5, loops);
@@ -105,15 +157,13 @@ class ElementConditionTest {
         Element element = document.createElement("test");
         element.setAttribute("value", "1000");
         element.setAttribute("operation", "EQUALS");
-        Assertions.assertThrows(IllegalStateException.class,
-                () -> new ElementCondition(element, streamConfig).condition()
-        );
+        Assertions
+                .assertThrows(IllegalStateException.class, () -> new ElementCondition(element, streamConfig).condition());
         Element element2 = document.createElement("hostindex");
         element2.setAttribute("value", "test");
         element2.setAttribute("operation", "EQUALS");
-        Assertions.assertThrows(IllegalStateException.class,
-                () -> new ElementCondition(element2, streamConfig).condition()
-        );
+        Assertions
+                .assertThrows(IllegalStateException.class, () -> new ElementCondition(element2, streamConfig).condition());
     }
 
     @Test
