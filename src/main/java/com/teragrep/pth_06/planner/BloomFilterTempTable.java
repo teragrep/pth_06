308d34fe0caba3b01bfdccd8dc26b8112d4f2f77
elliVM <47@teragrep.com>
2024-09-06 09:26:12 +0300

    remove BloomFilterTempTable cache add equals method and equality tests

--- src/main/java/com/teragrep/pth_06/planner/BloomFilterTempTable.java
+++ src/main/java/com/teragrep/pth_06/planner/BloomFilterTempTable.java
@@ -1,6 +1,6 @@
 /*
- * This program handles user requests that require archive access.
- * Copyright (C) 2022, 2023, 2024  Suomen Kanuuna Oy
+ * Teragrep Archive Datasource (pth_06)
+ * Copyright (C) 2021-2024 Suomen Kanuuna Oy
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as published by
@@ -13,7 +13,7 @@
  * GNU Affero General Public License for more details.
  *
  * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://github.com/teragrep/teragrep/blob/main/LICENSE>.
+ * along with this program.  If not, see <https://www.gnu.org/licenses/>.
  *
  *
  * Additional permission under GNU Affero General Public License version 3
@@ -43,7 +43,6 @@
  * Teragrep, the applicable Commercial License may apply to this file if you as
  * a licensee so wish it.
  */
-
 package com.teragrep.pth_06.planner;
 
 import com.teragrep.blf_01.Token;
@@ -57,163 +56,168 @@ import org.slf4j.LoggerFactory;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.UncheckedIOException;
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Set;
 
 import static com.teragrep.pth_06.jooq.generated.bloomdb.Bloomdb.BLOOMDB;
-import static org.jooq.impl.SQLDataType.*;
+import static org.jooq.impl.SQLDataType.BIGINTUNSIGNED;
 
 /**
- * SQL Temp table filled with bloom filters of the parent table filter types, search term token set is
- * inserted into the temp table filter columns. Each row of the parent table can be compared against this temp table.
+ * SQL Temp table filled with bloom filters of the parent table filter types, search term token set is inserted into the
+ * temp table filter columns. Each row of the parent table can be compared against this temp table.
  * <p>
  * Parent Table Schema:
  * <ol>
- *     <li>id PK</li>
- *     <li>partition_id FK journaldb.logfile.id</li>
- *     <li>filter_type_id FK bloomdb.filtertype.id</li>
- *     <li>filter - bloomfilter byte array</li>
+ * <li>id PK</li>
+ * <li>partition_id FK journaldb.logfile.id</li>
+ * <li>filter_type_id FK bloomdb.filtertype.id</li>
+ * <li>filter - bloomfilter byte array</li>
  * </ol>
  * <p>
  * Temp Table Schema:
  * <ol>
- *     <li>id PK</li>
- *     <li>termId - count of the current search term</li>
- *     <li>typeId FK bloomdb.filtertype.id</li>
- *     <li>filter - bloomfilter bytes with only the search term token set inserted</li>
+ * <li>id PK</li>
+ * <li>termId - count of the current search term</li>
+ * <li>typeId FK bloomdb.filtertype.id</li>
+ * <li>filter - bloomfilter bytes with only the search term token set inserted</li>
  * </ol>
  * Parent table create table example:
  * <p>
  * <code>
  *     CREATE TABLE `example` ( `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT, `partition_id` bigint(20) unsigned NOT NULL, `filter_type_id` bigint(20) unsigned NOT NULL, `filter` longblob NOT NULL, PRIMARY KEY (`id`), UNIQUE KEY `partition_id` (`partition_id`), KEY `example_ibfk_1` (`filter_type_id`), CONSTRAINT `example_ibfk_1` FOREIGN KEY (`filter_type_id`) REFERENCES `filtertype` (`id`) ON DELETE CASCADE, CONSTRAINT `example_ibfk_2` FOREIGN KEY (`partition_id`) REFERENCES `journaldb`.`logfile` (`id`) ON DELETE CASCADE ) ENGINE=InnoDB AUTO_INCREMENT=54787 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci
  * </code>
+ *
  * @see com.teragrep.pth_06.jooq.generated.bloomdb.Bloomdb
  */
 public final class BloomFilterTempTable {
+
     private static final Logger LOGGER = LoggerFactory.getLogger(BloomFilterTempTable.class);
 
     private final DSLContext ctx;
     private final Table<?> parentTable;
-    private final Table<Record> tableName;
+    private final Table<Record> table;
     private final long bloomTermId;
     private final Set<Token> tokenSet;
-    private final List<Condition> cache;
     // Table fields
-    private final Field<ULong> termId;
-    private final Field<ULong> typeId;
-    private final Field<byte[]> filter;
+    private final Field<ULong> termIdField;
+    private final Field<ULong> typeIdField;
+    private final Field<byte[]> filterField;
 
     public BloomFilterTempTable(DSLContext ctx, Table<?> parentTable, long bloomTermId, Set<Token> tokenSet) {
         this.ctx = ctx;
         this.parentTable = parentTable;
-        this.tableName = DSL.table(DSL.name(("term_" + bloomTermId + "_" + parentTable.getName())));
+        this.table = DSL.table(DSL.name(("term_" + bloomTermId + "_" + parentTable.getName())));
         this.bloomTermId = bloomTermId;
         this.tokenSet = tokenSet;
-        this.cache = new ArrayList<>(1);
-        this.termId = DSL.field("term_id", BIGINTUNSIGNED.nullable(false));
-        this.typeId = DSL.field("type_id", BIGINTUNSIGNED.nullable(false));
-        this.filter = DSL.field(DSL.name(tableName.getName(), "filter"), byte[].class);
+        this.termIdField = DSL.field("term_id", BIGINTUNSIGNED.nullable(false));
+        this.typeIdField = DSL.field("type_id", BIGINTUNSIGNED.nullable(false));
+        this.filterField = DSL.field(DSL.name(table.getName(), "filter"), byte[].class);
     }
 
     private void create() {
         if (LOGGER.isInfoEnabled()) {
-            LOGGER.info("Creating temporary table <{}>", tableName.getName());
+            LOGGER.info("Creating temporary table <{}>", table.getName());
         }
-        ctx.dropTemporaryTableIfExists(tableName).execute();
-        String sql = "create temporary table " + tableName.getName() + "(id bigint auto_increment primary key, term_id bigint, type_id bigint, filter longblob, unique key " + tableName.getName() + "_unique_key (term_id, type_id))";
+        ctx.dropTemporaryTableIfExists(table).execute();
+        String sql = "create temporary table " + table.getName()
+                + "(id bigint auto_increment primary key, term_id bigint, type_id bigint, filter longblob, unique key "
+                + table.getName() + "_unique_key (term_id, type_id))";
         Query query = ctx.query(sql);
         query.execute();
-        Index logtimeIndex = DSL.index(DSL.name(tableName.getName() + "_ix_type_id"));
-        CreateIndexIncludeStep createIndexIncludeStep = ctx.createIndex(logtimeIndex).on(tableName, typeId);
+        Index logtimeIndex = DSL.index(DSL.name(table.getName() + "_ix_type_id"));
+        CreateIndexIncludeStep createIndexIncludeStep = ctx.createIndex(logtimeIndex).on(table, typeIdField);
         LOGGER.trace("BloomFilterTempTable create index <{}>", createIndexIncludeStep);
         createIndexIncludeStep.execute();
     }
 
     /**
-     * Inserts a record for each filtertype inside parent table into temp table.
-     * Filter is filled with search term token set and
-     * its size is selected using parent table filtertype expected and fpp values.
+     * Inserts a record for each filtertype inside parent table into temp table. Filter is filled with search term token
+     * set and its size is selected using parent table filtertype expected and fpp values.
      */
     private void insertFilters() {
         Table<?> joined = parentTable;
-        joined = joined.join(BLOOMDB.FILTERTYPE).on(BLOOMDB.FILTERTYPE.ID.eq(
-                        (Field<ULong>) parentTable.field("filter_type_id")
-                )
-        );
-        final Field<ULong> expectedField = DSL.field(DSL.name(tableName.getName(), "expectedElements"), ULong.class);
-        final Field<Double> fppField = DSL.field(DSL.name(tableName.getName(), "targetFpp"), Double.class);
+        joined = joined
+                .join(BLOOMDB.FILTERTYPE)
+                .on(BLOOMDB.FILTERTYPE.ID.eq((Field<ULong>) parentTable.field("filter_type_id")));
+        final Field<ULong> expectedField = DSL.field(DSL.name(table.getName(), "expectedElements"), ULong.class);
+        final Field<Double> fppField = DSL.field(DSL.name(table.getName(), "targetFpp"), Double.class);
         final SelectField<?>[] resultFields = {
                 BLOOMDB.FILTERTYPE.ID,
                 joined.field("expectedElements").as(expectedField),
                 joined.field("targetFpp").as(fppField)
         };
         // Fetch filtertype values
-        Result<Record> records = ctx
-                .select(resultFields)
-                .from(joined)
-                .groupBy(joined.field("filter_type_id"))
-                .fetch();
+        Result<Record> records = ctx.select(resultFields).from(joined).groupBy(joined.field("filter_type_id")).fetch();
         if (records.isEmpty()) {
             throw new RuntimeException("Parent table was empty");
         }
         for (Record record : records) {
             ULong filterTypeId = record.getValue(BLOOMDB.FILTERTYPE.ID); // filter_type_id
-            ULong expected = record.getValue(expectedField);  // expectedElements
-            Double fpp = record.getValue(fppField);     // targetFpp
+            ULong expected = record.getValue(expectedField); // expectedElements
+            Double fpp = record.getValue(fppField); // targetFpp
             BloomFilter filter = BloomFilter.create(expected.longValue(), fpp);
             tokenSet.forEach(token -> filter.put(token.toString()));
-
-            final ByteArrayOutputStream filterBAOS = new ByteArrayOutputStream();
-            try {
-                filter.writeTo(filterBAOS);
-                filterBAOS.close();
-            } catch (IOException e) {
-                throw new UncheckedIOException(e);
-            }
-            ctx.insertInto(tableName).columns(
-                            this.termId,
-                            this.typeId,
-                            this.filter
-                    ).values(
-                            DSL.val(bloomTermId, ULong.class),
-                            DSL.val(filterTypeId, ULong.class),
-                            DSL.val(filterBAOS.toByteArray(), byte[].class)
-                    )
-                    .execute();
+            insertOneFilter(filter, filterTypeId);
         }
     }
 
+    private void insertOneFilter(BloomFilter filter, ULong filterTypeId) {
+        final ByteArrayOutputStream filterBAOS = new ByteArrayOutputStream();
+        try {
+            filter.writeTo(filterBAOS);
+            filterBAOS.close();
+        }
+        catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+        ctx
+                .insertInto(table)
+                .columns(this.termIdField, this.typeIdField, this.filterField)
+                .values(DSL.val(bloomTermId, ULong.class), DSL.val(filterTypeId, ULong.class), DSL.val(filterBAOS.toByteArray(), byte[].class)).execute();
+    }
+
     /**
-     * Generates a condition that returns true if this temp tables search term tokens
-     * might be contained in the parent table or parent table bloom filter is null.
-     * Selects to same sized filter from the temp table for each parent table row.
+     * Generates a condition that returns true if this temp tables search term tokens might be contained in the parent
+     * table or parent table bloom filter is null. Selects to same sized filter from the temp table for each parent
+     * table row.
      * <p>
      * expects the user defined function 'bloommatch' to be present to compare filter bytes
      *
      * @return Condition - generated condition
      */
     public Condition generateCondition() {
-        if (cache.isEmpty()) {
-            create();
-            insertFilters();
-            SelectConditionStep<Record1<byte[]>> selectConditionStep =
-                    DSL.select(filter)
-                            .from(tableName)
-                            .where(
-                                    termId.eq(ULong.valueOf(bloomTermId))
-                            ).and(
-                                    typeId.eq((Field<ULong>) parentTable.field("filter_type_id"))
-                            );
-            Field<byte[]> termFilterColumn = selectConditionStep.asField();
-            Condition filterFieldCondition = DSL.function("bloommatch", Boolean.class,
-                    termFilterColumn,
-                    parentTable.field("filter")
-            ).eq(true);
-            // null check allows SQL to optimize query
-            Condition notNullCondition = parentTable.field("filter").isNotNull();
-            cache.add(filterFieldCondition.and(notNullCondition));
-        }
-        return cache.get(0);
+        create();
+        insertFilters();
+        SelectConditionStep<Record1<byte[]>> selectConditionStep = DSL
+                .select(filterField)
+                .from(table)
+                .where(termIdField.eq(ULong.valueOf(bloomTermId)))
+                .and(typeIdField.eq((Field<ULong>) parentTable.field("filter_type_id")));
+        Field<byte[]> termFilterColumn = selectConditionStep.asField();
+        Condition filterFieldCondition = DSL
+                .function("bloommatch", Boolean.class, termFilterColumn, parentTable.field("filter"))
+                .eq(true);
+        // null check allows SQL to optimize query
+        Condition notNullCondition = parentTable.field("filter").isNotNull();
+
+        return filterFieldCondition.and(notNullCondition);
+    }
+
+    /**
+     * Equal only if same instance of DSLContext
+     * 
+     * @param object object compared against
+     * @return true if all object is same class, object fields are equal and DSLContext is same instance
+     */
+    @Override
+    public boolean equals(final Object object) {
+        if (this == object)
+            return true;
+        if (object == null)
+            return false;
+        if (object.getClass() != this.getClass())
+            return false;
+        final BloomFilterTempTable cast = (BloomFilterTempTable) object;
+        return this.parentTable.equals(cast.parentTable) && this.table.equals(cast.table) && this.ctx == cast.ctx && // equal only if same instance of DSLContext
+                this.bloomTermId == cast.bloomTermId && this.tokenSet.equals(cast.tokenSet);
     }
 }
