948c2da08907a42699ce45eaa285b1527cdb9ce3
elliVM <47@teragrep.com>
2024-09-09 09:47:35 +0300

    Revert "check for year formating in license script, run license script and apply spotless"
    This reverts commit 7dd5e3339f9996999a4000a38e9ee95df9f97aa0.

--- add_headers_to_generated.sh
+++ add_headers_to_generated.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 find src/main/java/com/teragrep/pth_06/jooq/generated -type f -name "*.java" -print0 | while read -r -d $'\0' file
 do
-    if ! grep -q "Copyright (C) .* Suomen Kanuuna Oy 2021-" "${file}"; then
+    if ! grep -q "Copyright (C) .* Suomen Kanuuna Oy" "${file}"; then
         cat license-header > "${file}.tmp";
         cat "${file}" >> "${file}.tmp";
         mv -f "${file}.tmp" "${file}";
