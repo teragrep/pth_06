5c816404154548f90d2bec891f525a993f30e5e1
Tiihott <48@teragrep.com>
2024-09-19 15:51:38 +0300

    Moved FileSystem initialization logic from constructors of HdfsDBClient and HdfsMicroBatchInputPartitionReader to FileSystemFactoryImpl.

--- src/main/java/com/teragrep/pth_06/planner/HdfsDBClient.java
+++ src/main/java/com/teragrep/pth_06/planner/HdfsDBClient.java
@@ -45,18 +45,15 @@
  */
 package com.teragrep.pth_06.planner;
 
+import com.teragrep.pth_06.FileSystemFactoryImpl;
 import com.teragrep.pth_06.config.Config;
 import com.teragrep.pth_06.HdfsTopicPartitionOffsetMetadata;
-import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.*;
-import org.apache.hadoop.hdfs.DistributedFileSystem;
-import org.apache.hadoop.security.UserGroupInformation;
 import org.apache.kafka.common.TopicPartition;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.IOException;
-import java.net.URI;
 import java.util.LinkedList;
 
 // Searches the HDFS database for files contained in directories based on topicsRegexString. Creates a list of objects where each object contains the metadata of a single HDFS-file.
@@ -65,79 +62,14 @@ public class HdfsDBClient {
     private final Logger LOGGER = LoggerFactory.getLogger(HdfsDBClient.class);
     private final String path;
     private final FileSystem fs;
-    private final Configuration conf;
-    private final String hdfsuri;
     private final String topicsRegexString;
     private final long ignoreBeforeEpoch;
 
     public HdfsDBClient(Config config, String topicsRegexString) throws IOException {
         this.topicsRegexString = topicsRegexString;
         this.ignoreBeforeEpoch = config.hdfsConfig.includeFileEpochAndAfter; // Defines the minimum time window / cutoff epoch for making sure that the files which metadata is fetched by the planner are not mistakenly deleted by cfe_39 pruning before tasker can process the records inside.
-        // Implement HDFS FileSystem access here
-        if ("kerberos".equals(config.hdfsConfig.kerberosAuthentication)) {
-            // Code for initializing the class with kerberos.
-            hdfsuri = config.hdfsConfig.hdfsUri; // Get from config.
-
-            /* The filepath should be something like hdfs:///opt/teragrep/cfe_39/srv/topic_name/0.12345 where 12345 is offset and 0 the partition.
-             In other words the directory named topic_name holds files that are named and arranged based on partition and the partition's offset. Every partition has its own set of unique offset values.
-             The values are fetched from config and input parameters (topic+partition+offset).*/
-            path = config.hdfsConfig.hdfsPath;
-
-            // set kerberos host and realm
-            System.setProperty("java.security.krb5.realm", config.hdfsConfig.kerberosRealm);
-            System.setProperty("java.security.krb5.kdc", config.hdfsConfig.kerberosKdc);
-
-            conf = new Configuration();
-
-            // enable kerberus
-            conf.set("hadoop.security.authentication", config.hdfsConfig.kerberosAuthentication);
-            conf.set("hadoop.security.authorization", config.hdfsConfig.kerberosAuthorization);
-            conf.set("hadoop.kerberos.keytab.login.autorenewal.enabled", config.hdfsConfig.kerberosAutorenewal);
-
-            conf.set("fs.defaultFS", hdfsuri); // Set FileSystem URI
-            conf.set("fs.hdfs.impl", DistributedFileSystem.class.getName()); // Maven stuff?
-            conf.set("fs.file.impl", LocalFileSystem.class.getName()); // Maven stuff?
-
-            // hack for running locally with fake DNS records, set this to true if overriding the host name in /etc/hosts
-            conf.set("dfs.client.use.datanode.hostname", config.hdfsConfig.UseHdfsHostname);
-
-            // server principal, the kerberos principle that the namenode is using
-            conf.set("dfs.namenode.kerberos.principal.pattern", config.hdfsConfig.kerberosPrincipalPattern);
-
-            conf.set("dfs.data.transfer.protection", config.hdfsConfig.hdfsTransferProtection);
-            conf.set("dfs.encrypt.data.transfer.cipher.suites", config.hdfsConfig.hdfsCipherSuites);
-
-            // set usergroup stuff
-            UserGroupInformation.setConfiguration(conf);
-            UserGroupInformation
-                    .loginUserFromKeytab(config.hdfsConfig.kerberosKeytabUser, config.hdfsConfig.kerberosKeytabPath);
-
-            // filesystem for HDFS access is set here
-            fs = FileSystem.get(conf);
-        }
-        else {
-            // Code for initializing the class in test mode without kerberos.
-            hdfsuri = config.hdfsConfig.hdfsUri; // Get from config.
-
-            /* The filepath should be something like hdfs:///opt/teragrep/cfe_39/srv/topic_name/0.12345 where 12345 is offset and 0 the partition.
-             In other words the directory named topic_name holds files that are named and arranged based on partition and the partition's offset. Every partition has its own set of unique offset values.
-             These values should be fetched from config and other input parameters (topic+partition+offset).*/
-            path = config.hdfsConfig.hdfsPath; // Example path: hdfs://localhost:34509/opt/teragrep/cfe_39/srv/
-
-            // ====== Init HDFS File System Object
-            conf = new Configuration();
-            // Set FileSystem URI
-            conf.set("fs.defaultFS", hdfsuri);
-            // Because of Maven
-            conf.set("fs.hdfs.impl", DistributedFileSystem.class.getName());
-            conf.set("fs.file.impl", LocalFileSystem.class.getName());
-            // Set HADOOP user here, Kerberus parameters most likely needs to be added here too.
-            System.setProperty("HADOOP_USER_NAME", "hdfs");
-            System.setProperty("hadoop.home.dir", "/");
-            // filesystem for HDFS access is set here
-            fs = FileSystem.get(URI.create(hdfsuri), conf);
-
-        }
+        path = config.hdfsConfig.hdfsPath;
+        fs = new FileSystemFactoryImpl(config.hdfsConfig).create(true);
     }
 
     // this queries and pulls the distinct file metadata values to the partitionList according to the given query conditions (condition only applies to topic names in planner side).
