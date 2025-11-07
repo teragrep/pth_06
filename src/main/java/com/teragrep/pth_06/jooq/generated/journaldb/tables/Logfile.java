2f78d000f9af0bb0f0a6c297fc0e398d325d9571
Tiihott <48@teragrep.com>
2025-11-07 12:26:12 +0200

    Updated jooq auto-generated code with new schema. REBASE

--- src/main/java/com/teragrep/pth_06/jooq/generated/journaldb/tables/Logfile.java
+++ src/main/java/com/teragrep/pth_06/jooq/generated/journaldb/tables/Logfile.java
@@ -67,7 +67,7 @@ import org.jooq.Identity;
 import org.jooq.Index;
 import org.jooq.Name;
 import org.jooq.Record;
-import org.jooq.Row20;
+import org.jooq.Row19;
 import org.jooq.Schema;
 import org.jooq.Table;
 import org.jooq.TableField;
@@ -91,7 +91,7 @@ import org.jooq.types.UShort;
 @SuppressWarnings({ "all", "unchecked", "rawtypes" })
 public class Logfile extends TableImpl<LogfileRecord> {
 
-    private static final long serialVersionUID = 1081606291;
+    private static final long serialVersionUID = 418727679;
 
     /**
      * The reference instance of <code>journaldb.logfile</code>
@@ -167,11 +167,6 @@ public class Logfile extends TableImpl<LogfileRecord> {
     public final TableField<LogfileRecord, String> ARCHIVE_ETAG = createField(DSL.name("archive_etag"), org.jooq.impl.SQLDataType.VARCHAR(64).nullable(false), this, "Object storage's MD5 hash of the log file (Note: room left for possible implementation changes)");
 
     /**
-     * The column <code>journaldb.logfile.logtag</code>. A link back to CFEngine
-     */
-    public final TableField<LogfileRecord, String> LOGTAG = createField(DSL.name("logtag"), org.jooq.impl.SQLDataType.VARCHAR(48).nullable(false), this, "A link back to CFEngine");
-
-    /**
      * The column <code>journaldb.logfile.source_system_id</code>. Log file's source system (references source_system.id)
      */
     public final TableField<LogfileRecord, UShort> SOURCE_SYSTEM_ID = createField(DSL.name("source_system_id"), org.jooq.impl.SQLDataType.SMALLINTUNSIGNED.nullable(false), this, "Log file's source system (references source_system.id)");
@@ -204,7 +199,7 @@ public class Logfile extends TableImpl<LogfileRecord> {
     /**
      * The column <code>journaldb.logfile.logtag_id</code>. Log file's foreign key to logtag
      */
-    public final TableField<LogfileRecord, ULong> LOGTAG_ID = createField(DSL.name("logtag_id"), org.jooq.impl.SQLDataType.BIGINTUNSIGNED.defaultValue(org.jooq.impl.DSL.field("NULL", org.jooq.impl.SQLDataType.BIGINTUNSIGNED)), this, "Log file's foreign key to logtag");
+    public final TableField<LogfileRecord, ULong> LOGTAG_ID = createField(DSL.name("logtag_id"), org.jooq.impl.SQLDataType.BIGINTUNSIGNED.nullable(false), this, "Log file's foreign key to logtag");
 
     /**
      * Create a <code>journaldb.logfile</code> table reference
@@ -246,7 +241,7 @@ public class Logfile extends TableImpl<LogfileRecord> {
 
     @Override
     public List<Index> getIndexes() {
-        return Arrays.<Index>asList(Indexes.LOGFILE_BUCKET_ID, Indexes.LOGFILE_CATEGORY_ID, Indexes.LOGFILE_CIX_LOGFILE_EPOCH_HOUR_HOST_ID_LOGTAG, Indexes.LOGFILE_CIX_LOGFILE_HOST_ID_LOGTAG_LOGDATE, Indexes.LOGFILE_CIX_LOGFILE_LOGDATE_HOST_ID_LOGTAG, Indexes.LOGFILE_IX_LOGFILE_EPOCH_EXPIRES, Indexes.LOGFILE_IX_LOGFILE_EXPIRATION, Indexes.LOGFILE_IX_LOGFILE__SOURCE_SYSTEM_ID, Indexes.LOGFILE_LOGTAG_ID, Indexes.LOGFILE_PRIMARY, Indexes.LOGFILE_UIX_LOGFILE_OBJECT_HASH);
+        return Arrays.<Index>asList(Indexes.LOGFILE_BUCKET_ID, Indexes.LOGFILE_CATEGORY_ID, Indexes.LOGFILE_CIX_LOGFILE_EPOCH_HOUR_HOST_ID_LOGTAG_ID, Indexes.LOGFILE_CIX_LOGFILE_HOST_ID_LOGTAG_ID_LOGDATE, Indexes.LOGFILE_CIX_LOGFILE_LOGDATE_HOST_ID_LOGTAG_ID, Indexes.LOGFILE_FK_LOGFILE__LOGTAG_ID, Indexes.LOGFILE_IX_LOGFILE_EPOCH_EXPIRES, Indexes.LOGFILE_IX_LOGFILE_EXPIRATION, Indexes.LOGFILE_IX_LOGFILE__SOURCE_SYSTEM_ID, Indexes.LOGFILE_PRIMARY, Indexes.LOGFILE_UIX_LOGFILE_OBJECT_HASH);
     }
 
     @Override
@@ -308,11 +303,11 @@ public class Logfile extends TableImpl<LogfileRecord> {
     }
 
     // -------------------------------------------------------------------------
-    // Row20 type methods
+    // Row19 type methods
     // -------------------------------------------------------------------------
 
     @Override
-    public Row20<ULong, Date, Date, UShort, String, String, UShort, String, Timestamp, ULong, String, String, String, UShort, UShort, ULong, ULong, ULong, ULong, ULong> fieldsRow() {
-        return (Row20) super.fieldsRow();
+    public Row19<ULong, Date, Date, UShort, String, String, UShort, String, Timestamp, ULong, String, String, UShort, UShort, ULong, ULong, ULong, ULong, ULong> fieldsRow() {
+        return (Row19) super.fieldsRow();
     }
 }
