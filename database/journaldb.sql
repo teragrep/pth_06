6143257331d417853d094a3dfb787020da80cfa1
Tiihott <48@teragrep.com>
2025-11-12 10:03:42 +0200

    Updated journaldb.sql with schema change for CI normalized table. REBASE

--- database/journaldb.sql
+++ database/journaldb.sql
@@ -110,6 +110,30 @@ LOCK TABLES `category` WRITE;
 UNLOCK TABLES;
 
 --
+-- Table structure for table `ci`
+--
+
+DROP TABLE IF EXISTS `ci`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8mb4 */;
+CREATE TABLE `ci` (
+  `id` bigint(20) unsigned NOT NULL AUTO_INCREMENT COMMENT 'ID for ci',
+  `name` varchar(255) NOT NULL COMMENT 'Configuration item name of the logfile records',
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `uix_ci` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci ROW_FORMAT=DYNAMIC COMMENT='Contains ci values that are identified using the ID';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `ci`
+--
+
+LOCK TABLES `ci` WRITE;
+/*!40000 ALTER TABLE `ci` DISABLE KEYS */;
+/*!40000 ALTER TABLE `ci` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `corrupted_archive`
 --
 
@@ -216,6 +240,7 @@ CREATE TABLE `logfile` (
   `epoch_expires` bigint(20) unsigned DEFAULT NULL COMMENT 'Log file''s  epoch expiration',
   `epoch_archived` bigint(20) unsigned DEFAULT NULL COMMENT 'Log file''s  epoch archived',
   `logtag_id` bigint(20) unsigned NOT NULL COMMENT 'Log file''s foreign key to logtag',
+  `ci_id` bigint(20) unsigned DEFAULT NULL COMMENT 'Log file''s foreign key to ci table',
   PRIMARY KEY (`id`),
   UNIQUE KEY `uix_logfile_object_hash` (`object_key_hash`),
   KEY `bucket_id` (`bucket_id`),
@@ -229,6 +254,8 @@ CREATE TABLE `logfile` (
   KEY `fk_logfile__logtag_id` (`logtag_id`),
   KEY `cix_logfile_epoch_hour_host_id_logtag_id` (`epoch_hour`,`host_id`,`logtag_id`),
   KEY `cix_logfile_logdate_host_id_logtag_id` (`logdate`,`host_id`,`logtag_id`),
+  KEY `fk_logfile__ci_id` (`ci_id`),
+  CONSTRAINT `fk_logfile__ci_id` FOREIGN KEY (`ci_id`) REFERENCES `ci` (`id`),
   CONSTRAINT `fk_logfile__logtag_id` FOREIGN KEY (`logtag_id`) REFERENCES `logtag` (`id`),
   CONSTRAINT `fk_logfile__source_system_id` FOREIGN KEY (`source_system_id`) REFERENCES `source_system` (`id`),
   CONSTRAINT `logfile_ibfk_1` FOREIGN KEY (`bucket_id`) REFERENCES `bucket` (`id`),
